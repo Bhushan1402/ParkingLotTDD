e021ba73416bd2021c5dd05ff7cbdac5950d9cda
Bhushan <bhushan1402>
2020-05-05 11:47:57 +0530

    [Bhushan]Added:Test Case To Accommodate Changes As Per Main Method

--- src/test/java/com/bridgelabz/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/ParkingLotTest.java
@@ -8,18 +8,38 @@ public class ParkingLotTest {
     ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
 
     @Test
-    public void givenAVehicleToPArk_WhenParkedInParkingLot_ShouldReturnTrue() {
+    public void givenAVehicleToPArk_WhenParkedInParkingLot_ShouldReturnTrue() throws ParkingLotException {
         Object vehicle = new Object();
         boolean park = parkingLotSystem.isPark(vehicle);
         Assert.assertTrue(park);
     }
 
     @Test
-    public void givenAVehicle_WhenUnParkedFromParkingLot_ShouldReturnTrue() {
+    public void givenAVehicle_WhenUnParkedFromParkingLot_ShouldReturnTrue() throws ParkingLotException {
         Object vehicle = new Object();
         boolean unPark = parkingLotSystem.unParkTheVehicle(vehicle);
         Assert.assertTrue(unPark);
     }
 
+    @Test
+    public void givenAVehicleToPark_WhenThereAreOtherVehicles_ShouldReturnTrue() throws ParkingLotException {
+        Object vehicleOne = new Object();
+        Object vehicleTwo = new Object();
+        parkingLotSystem.isPark(vehicleOne);
+        boolean park = parkingLotSystem.isPark(vehicleTwo);
+        Assert.assertTrue(park);
+    }
 
+    @Test
+    public void givenAVehicle_WhenNotPresentInParkingLot_ShouldReturnException() throws ParkingLotException {
+        Object vehicleOne = new Object();
+        Object vehicle = new Object();
+        parkingLotSystem.isPark(vehicleOne);
+        boolean unPark = false;
+        try {
+            unPark = parkingLotSystem.unParkTheVehicle(vehicle);
+        } catch (ParkingLotException e) {
+            Assert.assertEquals("Vehicle Is Not Parked Here",e.getMessage());
+        }
+    }
 }
