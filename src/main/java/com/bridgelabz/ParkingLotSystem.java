0a211d6aa84c5b08b5644b2601deaf2807191aea
Bhushan <bhushan1402>
2020-05-05 10:47:18 +0530

    [Bhushan]Added:Method To Park The Vehicle and Test Case For Parking The Vehicle

--- src/main/java/com/bridgelabz/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/ParkingLotSystem.java
@@ -4,4 +4,14 @@ public class ParkingLotSystem {
     public static void main(String[] args) {
         System.out.println("Welcome To ParkingLot");
     }
+
+    private Object vehicle;
+
+    //METHOD TO PARK THE VEHICLE
+    public boolean isPark(Object vehicle) {
+        if (this.vehicle != null)
+            return false;
+        this.vehicle = vehicle;
+        return true;
+    }
 }
--- src/test/java/com/bridgelabz/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/ParkingLotTest.java
@@ -1,5 +1,16 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ParkingLotTest {
 
+    ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+
+    @Test
+    public void givenAVehicleToPArk_WhenParkedInParkingLot_ShouldReturnTrue() {
+        Object vehicle = new Object();
+        boolean park = parkingLotSystem.isPark(vehicle);
+        Assert.assertTrue(park);
+    }
 }
