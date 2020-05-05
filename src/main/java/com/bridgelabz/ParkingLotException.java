bccfcd944233e8445992d9f0c33294905c0198c4
Bhushan <bhushan1402>
2020-05-05 11:01:25 +0530

    [Bhushan]Added:Code To Throw Exception And Find Size Of List

--- src/main/java/com/bridgelabz/ParkingLotException.java
+++ src/main/java/com/bridgelabz/ParkingLotException.java
@@ -4,10 +4,11 @@ public class ParkingLotException extends Exception {
 
     public ExceptionType type;
 
-    public ParkingLotException(ExceptionType type, String message){
+    public ParkingLotException(String message){
         super(message);
-        this.type=type;
     }
+
+
     public enum ExceptionType {
         PARKING_LOT_FULL, VEHICLE_ALREADY_UNPARKED_OR_WRONG_VEHICLE;
     }
--- src/main/java/com/bridgelabz/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/ParkingLotSystem.java
@@ -1,27 +1,33 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLotSystem {
     public static void main(String[] args) {
         System.out.println("Welcome To ParkingLot");
     }
 
-    private Object vehicle;
+    List<Object> vehicle = new ArrayList<Object>();
 
     //METHOD TO PARK THE VEHICLE
-    public boolean isPark(Object vehicle) {
-        if (this.vehicle != null)
-            return false;
-        this.vehicle = vehicle;
-        return true;
+    public boolean isPark(Object vehicleToPark) throws ParkingLotException {
+        if (this.vehicle.isEmpty()) {
+            vehicle.add(vehicleToPark);
+            return true;
+        } else if (!this.vehicle.isEmpty() && (!this.vehicle.contains(vehicleToPark)) && (this.vehicle.size() < 5)) {
+            vehicle.add(vehicleToPark);
+            return true;
+        } else
+            throw new ParkingLotException("Parking Lot Is Full");
     }
 
     //METHOD TO UNPARK THE GIVEN VEHICLE
-    public boolean unParkTheVehicle(Object vehicle) {
-        this.vehicle = vehicle;
-        if (this.vehicle.equals(vehicle)) {
-            this.vehicle = null;
+    public boolean unParkTheVehicle(Object vehicleToUnPark) throws ParkingLotException {
+        if (vehicle.contains(vehicleToUnPark)) {
+            vehicle.remove(vehicleToUnPark);
             return true;
         }
-        return false;
+        throw new ParkingLotException("Vehicle Is Not Parked Here");
     }
 }
